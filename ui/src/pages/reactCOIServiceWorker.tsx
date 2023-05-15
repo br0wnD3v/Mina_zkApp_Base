11dceb2441d98e094b6bd7b941c55f68f1197226
br0wnD3v <bothereum@protonmail.com>
2023-05-15 18:13:19 +0530

    Updates.

--- ui/src/pages/reactCOIServiceWorker.tsx
+++ ui/src/pages/reactCOIServiceWorker.tsx
@@ -1,10 +1,12 @@
-
-export {}
+export {};
 
 function loadCOIServiceWorker() {
-  if (typeof window !== 'undefined' && window.location.hostname != 'localhost') {
-    const coi = window.document.createElement('script');
-    coi.setAttribute('src','/basic_page/coi-serviceworker.min.js'); // update if your repo name changes for npm run deploy to work successfully
+  if (
+    typeof window !== "undefined" &&
+    window.location.hostname != "localhost"
+  ) {
+    const coi = window.document.createElement("script");
+    coi.setAttribute("src", "/basic_page/coi-serviceworker.min.js"); // update if your repo name changes for npm run deploy to work successfully
     window.document.head.appendChild(coi);
   }
 }
